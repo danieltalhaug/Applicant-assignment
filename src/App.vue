cf9e4502aeca4331d86916a59c0460342ecbe9a9
danieltalhaug <daniel.talhaug@outlook.com>
2020-12-12 12:31:26 +0100

    Created and imported Searchbar.vue in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Header/>
+    <Searchbar/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Searchbar from './components/Searchbar.vue'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Searchbar
   }
 }
 </script>
